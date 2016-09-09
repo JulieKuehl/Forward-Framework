112dd224def8927e5dc1100609fb4d17c276c7bc
juliekuehl <julie@juliekuehl.com>
2016-09-09 15:10:17 -0500

    added front-page and hero files

---
+++ src/content-hero.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<section class="hero-section" style="background-image: url( <?php the_post_thumbnail_url( 'full' ); ?> )">
+
+	<div class="hero-text">
+		<h3>Hero Title</h3>
+
+		<div class="photo-credit">
+			<a href="#photo_credit_link" target="_blank">Photo
+				credit: Photo credit info</a>
+		</div>
+
+	</div>
+</section>
