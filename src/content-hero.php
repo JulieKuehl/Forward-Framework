eccc3be58e5ee8781cacdfef003cea7153034cdc
juliekuehl <julie@juliekuehl.com>
2016-09-09 15:22:01 -0500

    hero image and text

--- src/content-hero.php
+++ src/content-hero.php
@@ -3,7 +3,7 @@
 <section class="hero-section" style="background-image: url( <?php the_post_thumbnail_url( 'full' ); ?> )">
 
 	<div class="hero-text">
-		<h3>Hero Title</h3>
+		<p>Hero Title</p>
 
 		<div class="photo-credit">
 			<a href="#photo_credit_link" target="_blank">Photo
--- src/front-page.php
+++ src/front-page.php
@@ -1,48 +1,35 @@
 <?php
+
 /**
- * The main template file.
+ * Template Name: Full Width
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package Forward
+ * Displays the page without a sidebar.
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="full-width">
+	<main id="main" class="site-main" role="main">
 
 		<?php get_template_part( 'content', 'hero' ); ?>
 
-		<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				<header class="entry-header">
+<!--					--><?php //the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
 
-			<?php endwhile; ?>
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div><!-- .entry-content -->
 
-			<?php forward_posts_navigation(); ?>
+			</article><!-- #post-## -->
 
-		<?php else : ?>
+		<?php endwhile; // end of the loop. ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
