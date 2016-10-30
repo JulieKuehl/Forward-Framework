1f288a7b75284553e07acca862e7a6d0776da8f8
juliekuehl <julie@juliekuehl.com>
2016-10-30 12:55:13 -0700

    update gulpfile, svg images, dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,25 @@
 
 // Project Variables
 //
-var project     = 'forward',
-    build       = 'build/',
-    dist        = 'dist/'+project+'/',
-    source      = 'src/'
-    lang        = 'languages/',
-    bower       = 'bower_components/',
-    url         = 'forward.local'
+var project = 'forward-framework-prod',
+	build = 'build/',
+	// dist        = 'dist/'+project+'/',
+	dist = '../' + project + '/',
+	source = 'src/'
+	lang = 'languages/',
+	bower = 'bower_components/',
+	url = 'forward-framework.dev'
 ;
 
 // Gulp Settings & Startup
 //
-var gulp        = require('gulp'),
-    sass        = require('gulp-ruby-sass'),
-    gutil       = require('gulp-util'),
-    plugins     = require('gulp-load-plugins')({ camelize: true }) // This loads all modules prefixed with "gulp-" to plugin.moduleName,
-    del         = require('del'),
-    browserSync = require('browser-sync'),
-    reload      = browserSync.reload;
+var gulp = require('gulp'),
+	sass = require('gulp-ruby-sass'),
+	gutil = require('gulp-util'),
+	plugins = require('gulp-load-plugins')({camelize: true}) // This loads all modules prefixed with "gulp-" to 	plugin.moduleName,
+	del = require('del'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload;
 ;
 
 
@@ -31,17 +32,17 @@ var gulp        = require('gulp'),
 
 // Process Stylesheets
 //
-gulp.task('styles', function() {
-  return sass(source + 'scss/forward/style.scss', {
-    style: 'expanded',
-    loadPath: bower
-  })
-  .pipe(plugins.autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
-  .pipe(plugins.mergeMediaQueries())
-  .pipe(plugins.minifyCss({ keepSpecialComments: 1, keepBreaks: true, compatibility: 'ie8' }))
-  .pipe(plugins.pixrem())
-  .pipe(gulp.dest(build))
-  .pipe(browserSync.reload({ stream: true }))
+gulp.task('styles', function () {
+	return sass(source + 'scss/forward/style.scss', {
+		style: 'expanded',
+		loadPath: bower
+	})
+	.pipe(plugins.autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
+	.pipe(plugins.mergeMediaQueries())
+	.pipe(plugins.minifyCss({keepSpecialComments: 1, keepBreaks: true, compatibility: 'ie8'}))
+	.pipe(plugins.pixrem())
+	.pipe(gulp.dest(build))
+	.pipe(browserSync.reload({stream: true}))
 });
 
 
@@ -51,41 +52,41 @@ gulp.task('styles', function() {
 
 // Scripts; broken out into different tasks to create specific bundles which are then compressed in place
 //
-gulp.task('scripts', ['scripts-lint', 'scripts-core', 'scripts-extras'], function(){
-  return gulp.src([build+'js/**/*.js', '!'+build+'js/**/*.min.js']) // Avoid recursive min.min.min.js
-  .pipe(plugins.rename({suffix: '.min'}))
-  .pipe(plugins.uglify())
-  .pipe(gulp.dest(build+'js/'));
+gulp.task('scripts', ['scripts-lint', 'scripts-core', 'scripts-extras'], function () {
+	return gulp.src([build + 'js/**/*.js', '!' + build + 'js/**/*.min.js']) // Avoid recursive min.min.min.js
+	.pipe(plugins.rename({suffix: '.min'}))
+	.pipe(plugins.uglify())
+	.pipe(gulp.dest(build + 'js/'));
 });
 
 // Lint scripts for errors and sub-optimal formatting
 //
-gulp.task('scripts-lint', function() {
-  return gulp.src(source+'js/**/*.js')
-  .pipe(plugins.jshint('.jshintrc'))
-  .pipe(plugins.jshint.reporter('default'));
+gulp.task('scripts-lint', function () {
+	return gulp.src(source + 'js/**/*.js')
+	.pipe(plugins.jshint('.jshintrc'))
+	.pipe(plugins.jshint.reporter('default'));
 });
 
 // These are the core custom scripts loaded on every page; pass an array to bundle several scripts in order
 //
-gulp.task('scripts-core', function() {
-  return gulp.src([
-    source+'js/core.js'
-    //, source+'js/navigation.js' // An example of how to add files to a bundle
-  ])
-  .pipe(plugins.concat('core.js'))
-  .pipe(gulp.dest(build+'js/'));
+gulp.task('scripts-core', function () {
+	return gulp.src([
+		source + 'js/core.js'
+		//, source+'js/navigation.js' // An example of how to add files to a bundle
+	])
+	.pipe(plugins.concat('core.js'))
+	.pipe(gulp.dest(build + 'js/'));
 });
 
 // An example task for extra scripts that aren't loaded on every page
 //
-gulp.task('scripts-extras', function() {
-  return gulp.src([
-    // You can also add dependencies from Bower components e.g.: bower+'dependency/dependency.js',
-    source+'js/extras.js'
-  ])
-  .pipe(plugins.concat('extras.js'))
-  .pipe(gulp.dest(build+'js/'));
+gulp.task('scripts-extras', function () {
+	return gulp.src([
+		// You can also add dependencies from Bower components e.g.: bower+'dependency/dependency.js',
+		source + 'js/extras.js'
+	])
+	.pipe(plugins.concat('extras.js'))
+	.pipe(gulp.dest(build + 'js/'));
 });
 
 
@@ -95,9 +96,9 @@ gulp.task('scripts-extras', function() {
 
 // Copy images; minification occurs during packaging
 //
-gulp.task('images', function() {
-  return gulp.src(source+'**/*(*.png|*.jpg|*.jpeg|*.gif)')
-  .pipe(gulp.dest(build));
+gulp.task('images', function () {
+	return gulp.src(source + '**/*(*.png|*.jpg|*.jpeg|*.gif|*.svg)')
+	.pipe(gulp.dest(build));
 });
 
 
@@ -107,11 +108,10 @@ gulp.task('images', function() {
 
 // Copy everything under `src/languages` indiscriminately
 //
-gulp.task('languages', function() {
-  return gulp.src(source+lang+'**/*')
-  .pipe(gulp.dest(build+lang));
+gulp.task('languages', function () {
+	return gulp.src(source + lang + '**/*')
+	.pipe(gulp.dest(build + lang));
 });
-
 
 
 /////////
@@ -120,9 +120,9 @@ gulp.task('languages', function() {
 
 // Copy PHP source files to the build directory
 //
-gulp.task('php', function() {
-  return gulp.src(source+'**/*.php')
-  .pipe(gulp.dest(build));
+gulp.task('php', function () {
+	return gulp.src(source + '**/*.php')
+	.pipe(gulp.dest(build));
 });
 
 
@@ -135,41 +135,41 @@ gulp.task('php', function() {
 
 // Clean out junk files after build
 //
-gulp.task('clean', ['build'], function(cb) {
-  del([build+'**/.DS_Store'], cb)
+gulp.task('clean', ['build'], function (cb) {
+	del([build + '**/.DS_Store'], cb)
 });
 
 // Totally wipe the contents of the distribution folder after doing a clean build
 //
-gulp.task('dist-wipe', ['clean'], function(cb) {
-  del([dist], cb)
+gulp.task('dist-wipe', ['clean'], function (cb) {
+	del([dist], cb)
 });
 
 // Copy everything in the build folder (except previously minified stylesheets) to the `dist/project` folder
 //
-gulp.task('dist-copy', ['dist-wipe'], function() {
-  return gulp.src([build+'**/*', '!'+build+'**/*.min.css'])
-  .pipe(gulp.dest(dist));
+gulp.task('dist-copy', ['dist-wipe'], function () {
+	return gulp.src([build + '**/*', '!' + build + '**/*.min.css'])
+	.pipe(gulp.dest(dist));
 });
 
 // Minify stylesheets in place
 //
-gulp.task('dist-styles', ['dist-copy'], function() {
-  return gulp.src([dist+'**/*.css', '!'+dist+'**/*.min.css'])
-  .pipe(plugins.minifyCss({ keepSpecialComments: 1, keepBreaks: true }))
-  .pipe(gulp.dest(dist));
+gulp.task('dist-styles', ['dist-copy'], function () {
+	return gulp.src([dist + '**/*.css', '!' + dist + '**/*.min.css'])
+	.pipe(plugins.minifyCss({keepSpecialComments: 1, keepBreaks: true}))
+	.pipe(gulp.dest(dist));
 });
 
 // Optimize images in place
 //
-gulp.task('dist-images', ['dist-styles'], function() {
-  return gulp.src([dist+'**/*.png', dist+'**/*.jpg', dist+'**/*.jpeg', dist+'**/*.gif', '!'+dist+'screenshot.png'])
-  .pipe(plugins.imagemin({
-    optimizationLevel: 7
-  , progressive: true
-  , interlaced: true
-  }))
-  .pipe(gulp.dest(dist));
+gulp.task('dist-images', ['dist-styles'], function () {
+	return gulp.src([dist + '**/*.png', dist + '**/*.jpg', dist + '**/*.jpeg', dist + '**/*.gif', '!' + dist + 'screenshot.png'])
+		.pipe(plugins.imagemin({
+		optimizationLevel: 7
+		, progressive: true
+		, interlaced: true
+		}))
+		.pipe(gulp.dest(dist));
 });
 
 
@@ -183,10 +183,10 @@ gulp.task('bower', ['bower-normalize']);
 
 // Used to get around Sass's inability to properly @import vanilla CSS
 //
-gulp.task('bower-normalize', function() {
-  return gulp.src(bower+'normalize.css/normalize.css')
-  .pipe(plugins.rename('_normalize.scss'))
-  .pipe(gulp.dest(source+'scss/base'));
+gulp.task('bower-normalize', function () {
+	return gulp.src(bower + 'normalize.css/normalize.css')
+	.pipe(plugins.rename('_normalize.scss'))
+	.pipe(gulp.dest(source + 'scss/base'));
 });
 
 
@@ -194,12 +194,12 @@ gulp.task('bower-normalize', function() {
 // Browser Sync //
 //////////////////
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        proxy: url
-        // Port setting for MAMP users
-        // , port: 8888
-    });
+gulp.task('browser-sync', function () {
+	browserSync({
+		proxy: url
+		// Port setting for MAMP users
+		// , port: 8888
+	});
 });
 
 
@@ -217,12 +217,12 @@ gulp.task('dist', ['dist-images']);
 
 // Watch Task
 //
-gulp.task('watch', ['browser-sync'], function() {
-  gulp.watch(source+'scss/**/*.scss', ['styles']);
-  gulp.watch([source+'js/**/*.js', bower+'**/*.js'], ['scripts']);
-  gulp.watch(source+'**/*(*.png|*.jpg|*.jpeg|*.gif)', ['images']);
-  gulp.watch(source+'**/*.php', ['php']);
-  // gulp.watch([build+'**/*', dist+'**/*'], reload);
+gulp.task('watch', ['browser-sync'], function () {
+	gulp.watch(source + 'scss/**/*.scss', ['styles']);
+	gulp.watch([source + 'js/**/*.js', bower + '**/*.js'], ['scripts']);
+	gulp.watch(source + '**/*(*.png|*.jpg|*.jpeg|*.gif)', ['images']);
+	gulp.watch(source + '**/*.php', ['php']);
+	// gulp.watch([build+'**/*', dist+'**/*'], reload);
 });
 
 // Default Task (Watch)
